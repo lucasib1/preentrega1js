e0b494180e11e415f9d91dd92efc151ebc4b0479
lucas ibañez <lucascompu2023@gmail.com>
2024-09-30 03:25:15 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+let recordatori =prompt("ingresar un recordatorio ");
+let diaDelRecordatorio =prompt ("ingresar dia del recordatorio ");
+let horaDelRecordatorio =parseInt(prompt("ingresar hora del recordatorio "));
+
+function recordatorio ( recordatorios , dia , hora ,){
+    console.log("tu recordatorio es" +" "+ recordatorios +" "+ "el dia"+" "+ dia+" "+ "a las "+" "+ hora)
+}
+recordatorio (recordatori,diaDelRecordatorio,horaDelRecordatorio)
+
+
